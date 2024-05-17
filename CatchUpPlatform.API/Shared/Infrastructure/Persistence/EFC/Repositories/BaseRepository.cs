fd70f3fb977cb0b6f15b4136aea53ca7398a89fb
Angel Velasquez <pcisavel@upc.edu.pe>
2024-05-17 17:08:30 -0500

    chore: added base outbound service.

---
+++ CatchUpPlatform.API/Shared/Infrastructure/Persistence/EFC/Repositories/BaseRepository.cs
@@ -0,0 +1,40 @@
+using CatchUpPlatform.API.Shared.Domain.Repositories;
+using CatchUpPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatchUpPlatform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+public abstract class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
+{
+    protected readonly AppDbContext Context;
+
+    protected BaseRepository(AppDbContext context)
+    {
+        Context = context;
+    }
+
+    public async Task AddAsync(TEntity entity)
+    {
+        await Context.Set<TEntity>().AddAsync(entity);
+    }
+
+    public async Task<TEntity?> FindByIdAsync(int id)
+    {
+        return await Context.Set<TEntity>().FindAsync(id);
+    }
+
+    public void Update(TEntity entity)
+    {
+        Context.Set<TEntity>().Update(entity);
+    }
+
+    public void Remove(TEntity entity)
+    {
+        Context.Set<TEntity>().Remove(entity);
+    }
+
+    public async Task<IEnumerable<TEntity>> ListAsync()
+    {
+        return await Context.Set<TEntity>().ToListAsync();
+    }
+}
