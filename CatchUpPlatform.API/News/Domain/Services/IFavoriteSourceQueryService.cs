5bea5ea5bc47da723e8560ae74f217973a5ad6f5
Angel Velasquez <pcisavel@upc.edu.pe>
2024-05-17 16:47:13 -0500

    feat(favorite-sources): added command, queries, handler contracts, and outbound service contracts.

---
+++ CatchUpPlatform.API/News/Domain/Services/IFavoriteSourceQueryService.cs
@@ -0,0 +1,11 @@
+using CatchUpPlatform.API.News.Domain.Model.Aggregates;
+using CatchUpPlatform.API.News.Domain.Model.Queries;
+
+namespace CatchUpPlatform.API.News.Domain.Services;
+
+public interface IFavoriteSourceQueryService
+{
+    Task<FavoriteSource> Handle(GetFavoriteSourceByIdQuery query);
+    Task<IEnumerable<FavoriteSource>> Handle(GetAllFavoriteSourcesByNewsApiKeyQuery query);
+    Task<FavoriteSource> Handle(GetFavoriteSourceByNewsApiKeyAndSourceIdQuery query);
+}
