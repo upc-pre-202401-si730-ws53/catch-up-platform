1229bd9ddc38ce93eebdba7030654fe923bac516
Angel Velasquez <pcisavel@upc.edu.pe>
2024-05-17 17:45:29 -0500

    feat(favorite-sources): added interface layer resources and assemblers for favorite source aggregate.

---
+++ CatchUpPlatform.API/News/Interfaces/REST/Transform/CreateFavoriteSourceCommandFromResourceAssembler.cs
@@ -0,0 +1,12 @@
+using CatchUpPlatform.API.News.Domain.Model.Commands;
+using CatchUpPlatform.API.News.Interfaces.REST.Resources;
+
+namespace CatchUpPlatform.API.News.Interfaces.REST.Transform;
+
+public static class CreateFavoriteSourceCommandFromResourceAssembler
+{
+    public static CreateFavoriteSourceCommand ToCommandFromResource(CreateFavoriteSourceResource resource)
+    {
+        return new CreateFavoriteSourceCommand(resource.NewsApiKey, resource.SourceId);
+    }
+}
---
+++ CatchUpPlatform.API/News/Interfaces/REST/Transform/FavoriteSourceResourceFromEntityAssembler.cs
@@ -0,0 +1,12 @@
+using CatchUpPlatform.API.News.Domain.Model.Aggregates;
+using CatchUpPlatform.API.News.Interfaces.REST.Resources;
+
+namespace CatchUpPlatform.API.News.Interfaces.REST.Transform;
+
+public static class FavoriteSourceResourceFromEntityAssembler
+{
+    public static FavoriteSourceResource ToResourceFromEntity(FavoriteSource entity)
+    {
+        return new FavoriteSourceResource(entity.Id, entity.NewsApiKey, entity.SourceId);
+    }
+}
