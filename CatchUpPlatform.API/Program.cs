c0a737b158dac13bf9514f1264472df0c51806d8
Angel Velasquez <pcisavel@upc.edu.pe>
2024-05-17 18:20:10 -0500

    chore: added database and application default configuration.

--- CatchUpPlatform.API/Program.cs
+++ CatchUpPlatform.API/Program.cs
@@ -1,13 +1,68 @@
+using CatchUpPlatform.API.News.Application.CommandServices;
+using CatchUpPlatform.API.News.Application.QueryServices;
+using CatchUpPlatform.API.News.Domain.Repositories;
+using CatchUpPlatform.API.News.Domain.Services;
+using CatchUpPlatform.API.News.Infrastructure.Persistence.EFC.Repositories;
+using CatchUpPlatform.API.Shared.Domain.Repositories;
+using CatchUpPlatform.API.Shared.Infrastructure.Interfaces.ASP.Configuration;
+using CatchUpPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using CatchUpPlatform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.Conventions.Add(new KebabCaseRouteNamingConvention());
+});
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Add Database Connection String
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Configure Database Context and Logging Level
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    if (connectionString != null)
+        if (builder.Environment.IsDevelopment())
+            options.UseMySQL(connectionString)
+                .LogTo(Console.WriteLine, LogLevel.Information)
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+        else if (builder.Environment.IsProduction())
+            options.UseMySQL(connectionString)
+                .LogTo(Console.WriteLine, LogLevel.Information)
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+});
+
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+// Configure Dependency Injections
+
+// Shared Bounded Context Dependency Injections
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+// News Bounded Context Dependency Injections
+builder.Services.AddScoped<IFavoriteSourceRepository, FavoriteSourceRepository>();
+builder.Services.AddScoped<IFavoriteSourceCommandService, FavoriteSourceCommandService>();
+builder.Services.AddScoped<IFavoriteSourceQueryService, FavoriteSourceQueryService>();
+
 var app = builder.Build();
+
+// Verify Database Objects are created
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
